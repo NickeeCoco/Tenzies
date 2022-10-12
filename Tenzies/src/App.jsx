5c93f998ad0221a6fe79dcb0d4c35a79d6ef83ae
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-10-12 16:15:54 -0400

    Create Die component + style

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,8 +1,21 @@
+import Die from "./Die"
+
 function App() {
 
   return (
     <main>
-      
+      <div className="diceContainer">
+        <Die value={8} />
+        <Die value={1} />
+        <Die value={9} />
+        <Die value={4} />
+        <Die value={2} />
+        <Die value={1} />
+        <Die value={3} />
+        <Die value={9} />
+        <Die value={7} />
+        <Die value={6} />
+      </div>
     </main>
   )
 }
