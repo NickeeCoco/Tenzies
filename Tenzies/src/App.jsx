6116342bdec59acdd77318c69432afabb8b1170f
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-10-13 12:25:17 -0400

    Check for a win

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,9 +1,10 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import Die from "./Die"
 import {nanoid} from "nanoid"
 
 function App() {
   const [dice, setDice] = useState(createNewDice())
+  const [tenzies, setTenzies] = useState(false)
 
   function generateNewDie() {
     return {
@@ -23,7 +24,34 @@ function App() {
     return newDice
   }
 
+  function holdDice(id) {
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? 
+        {...die, isHeld: !die.isHeld} :
+        die
+    }))
+  }
   
+  function rollDice() {
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ?
+        die :
+        generateNewDie()
+    }))
+  }
+
+  useEffect(() => {
+    const firstValue = dice[0].value
+    
+    const allHeld = dice.every(die => die.isHeld)
+    const allSameValue = dice.every(die => die.value === firstValue)
+    
+    if(allHeld && allSameValue) {
+      setTenzies(true)
+    }
+    
+  }, [dice])
+
   const diceElements = dice.map(die => 
     <Die 
       key={die.id} 
@@ -32,21 +60,7 @@ function App() {
     />
   )
 
-  function holdDice(id) {
-    setDice(prevDice => prevDice.map(die => {
-      return die.id === id ? 
-        {...die, isHeld: !die.isHeld} :
-        die
-    }))
-  }
-
-  function rollDice() {
-    setDice(prevDice => prevDice.map(die => {
-      return die.isHeld ?
-        die :
-        generateNewDie()
-    }))
-  }
+  
   
   return (
     <main>
