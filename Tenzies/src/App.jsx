d56eb1dca79dfdc8b46d401f5873526b7a58bb93
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-10-13 10:09:59 -0400

    Display dice dynamically
    Generate random numbers and use them to display dice.

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,20 +1,27 @@
+import {useState} from "react"
 import Die from "./Die"
 
 function App() {
+  const [numbers, setNumbers] = useState(allNewDice())
 
+  function allNewDice() {
+    const randomNumbers = []
+
+    for(let i = 0; i < 10; i++) {
+      const number = Math.ceil(Math.random() * 6)
+      randomNumbers.push(number)
+    }
+
+    return randomNumbers
+
+  }
+  
+  const dieElements = numbers.map(number => <Die value={number} />)
+  
   return (
     <main>
       <div className="diceContainer">
-        <Die value={8} />
-        <Die value={1} />
-        <Die value={9} />
-        <Die value={4} />
-        <Die value={2} />
-        <Die value={1} />
-        <Die value={3} />
-        <Die value={9} />
-        <Die value={7} />
-        <Die value={6} />
+        {dieElements}
       </div>
     </main>
   )
